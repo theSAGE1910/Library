00f221e988251226da90af91a2e51ad6e8e32dbc
theSAGE1910 <108081452+theSAGE1910@users.noreply.github.com>
2026-04-15 23:21:57 +0200

    v0.2

--- src/main/java/com/application/library/LibraryApplication.java
+++ src/main/java/com/application/library/LibraryApplication.java
@@ -1,4 +1,4 @@
-package com.application.Library;
+package com.application.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
