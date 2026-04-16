f2c0f5f959efb2b8ad708c7eb46cfb336aaec8a4
theSAGE1910 <108081452+theSAGE1910@users.noreply.github.com>
2026-04-17 00:19:27 +0200

    v0.6

--- src/main/java/com/application/library/controller/BookController.java
+++ src/main/java/com/application/library/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.application.library.controller;
 
 import com.application.library.entity.Book;
+import com.application.library.service.AuthorService;
 import com.application.library.service.BookService;
+import com.application.library.service.CategoryService;
+import com.application.library.service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,11 +18,48 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private PublisherService publisherService;
+    @Autowired
+    private AuthorService authorService;
 
     @GetMapping("/books")
     public String findAllBooks(Model model) {
         List<Book> books = bookService.findAllBooks();
         model.addAttribute("books", books);
         return "books";
+    }
+
+    @GetMapping("/book/{id}")
+    public String findBook(@PathVariable Long id, Model model) {
+        Book book = bookService.findBookById(id);
+        model.addAttribute("book", book);
+        return "list-book";
+    }
+
+    @GetMapping("remove-book/{id}")
+    public String deleteBook(@PathVariable Long id, Model model) {
+        bookService.deleteBookById(id);
+        model.addAttribute("books", bookService.findAllBooks());
+        return "books";
+    }
+
+    @GetMapping("add-book")
+    public String addBook(Book book, Model model) {
+        bookService.createBook(book);
+        model.addAttribute("books", bookService.findAllBooks());
+        return "books";
+    }
+
+    @GetMapping("update-book/{id}")
+    public String updateBook(@PathVariable Long id, Model model) {
+        Book book = bookService.findBookById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("categories", categoryService.findAllCategories());
+        model.addAttribute("publishers", publisherService.findAllPublishers());
+        model.addAttribute("authors", authorService.findAllAuthors());
+        return "update-book";
     }
 }
